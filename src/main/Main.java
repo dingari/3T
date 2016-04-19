cde7f284962477f816c0394afed182776ca2a9e5
Sykurpabbi <vjh2@hi.is>
2016-04-19 18:18:38 +0000

    icons

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -64,10 +66,17 @@ public class Main extends Application {
 
         // Top menu
         HBox topMenu = new HBox();
-        Button buttonFlights = new Button("Flight search");
-        Button buttonHotels = new Button("Hotel search");
-        Button buttonTours = new Button("Tour search");
-        Button buttonCart = new Button("View cart");
+
+        Image imageFlights = new Image(getClass().getResourceAsStream("iflights.png"));
+        Image imageHotels = new Image(getClass().getResourceAsStream("ihotels.png"));
+        Image imageTours = new Image(getClass().getResourceAsStream("itours.png"));
+        Image imageCart = new Image(getClass().getResourceAsStream("icart.png"));
+        Image imageSearch = new Image(getClass().getResourceAsStream("isearch.png"));
+
+        Button buttonFlights = new Button("Flight search",new ImageView(imageFlights));
+        Button buttonHotels = new Button("Hotel search",new ImageView(imageHotels));
+        Button buttonTours = new Button("Tour search", new ImageView(imageTours));
+        Button buttonCart = new Button("View cart", new ImageView(imageCart));
         topMenu.getChildren().addAll(buttonFlights,buttonHotels,buttonTours,buttonCart);
 
         /*
@@ -128,7 +137,7 @@ public class Main extends Application {
             System.out.println("Number of passengers: " + passengers);
         });
 
-        Button searchButton = new Button("Search");
+        Button searchButton = new Button("Search", new ImageView(imageSearch));
 
         // flights menu setup
         menuFlights.getChildren().addAll(
@@ -171,7 +180,6 @@ public class Main extends Application {
                 columnFlightAirportTo,
                 columnFlightPrice);
 
-        tableViewFlights.getSe
 
         // flights VIEW
 
@@ -309,6 +317,24 @@ public class Main extends Application {
         searchButton.setOnAction(e -> {
             System.out.println("button");
         });
+
+        // css style ids and more
+        mainFlights.setId("main");
+        mainHotels.setId("main");
+        mainTours.setId("main");
+        mainCart.setId("main");
+
+        topMenu.setId("topMenu");
+
+        buttonCart.setId("menuButton");
+        buttonFlights.setId("menuButton");
+        buttonHotels.setId("menuButton");
+        buttonTours.setId("menuButton");
+
+        menuFlights.setId("leftMenu");
+
+
+
 
         //setup
 
